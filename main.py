79cc8ab838858bd637755fab5e8c6c47d08b4ba2
JackCX777 <jackcx777@gmail.com>
2020-10-16 19:45:49 +0300

    gui done

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ import attack_plans
 # attack_plans.password_attack_by_common_list_only(substitution_password_attacks.brute_by_password_list)
 
 
-attack_plans.password_attack_by_brute_force_only(brute_force_password_attacks.brute_force_password)
+# attack_plans.password_attack_by_brute_force_only(brute_force_password_attacks.brute_force_password)
